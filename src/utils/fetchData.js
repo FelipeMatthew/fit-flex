94a30cc9c4f0c34db7222567c14b2eb2e317c104
felipematthew <felipematthewnascimento@gmail.com>
2024-06-16 14:46:06 -0300

    🐞 fix: Fixing API data settings

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,11 +4,13 @@ export const exerciseOptions = {
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
-}
+};
+
 
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
+  console.log(data);
 
   return data;
 };
